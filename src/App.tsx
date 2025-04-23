7651322f0e98210273b98563e509b6f82b9a8bd0
Henry Loveday <henrygad.orji@gmail.com>
2025-04-23 15:09:25 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import appleIcon from "./assets/icons8-apple-logo 1.png";
 const App = () => {
   return <div className="w-full min-h-screen flex flex-wrap gap-4 bg-white overflow-hidden">
     {/* sign up form section */}
-    <section className="flex-1 flex justify-center items-center pt-10">
+    <section className="flex-1 flex justify-center items-center pt-10 px-5 ">
       <div className="space-y-16">
-        <h2 className="font-text text-3xl font-bold">Get Started Now</h2>
+        <h2 className="font-text text-3xl font-medium">Get Started Now</h2>
         {/* sign up local form */}
         <form className="space-y-6">
           {/* client name */}
@@ -84,42 +84,38 @@ const App = () => {
             <span className="flex-1 border rounded-lg"></span> Or <span className="flex-1 border rounded-lg"></span>
           </span>
           {/* google and apple sign up login btn */}
-          <span className="flex justify-center items-center gap-2 mb-5">
+          <span className="flex justify-center items-center gap-6 mb-7">
             <button
               className="flex-1 flex justify-center items-center gap-2 p-1 border rounded-lg cursor-pointer"
             >
               <img
                 src={googleIcon}
                 alt="google icon"
-                className="h-5 w-5 object-contain"
+                className="h-6 w-6 object-contain"
               />
-              <span className="font-text text-xs font-medium text-nowrap" >Sign in with Google</span>
+              <span className="font-text text-[13px] font-medium text-nowrap" >Sign in with Google</span>
             </button>
             <button
-              className="flex-1 flex justify-center items-center gap-2 p-1 border rounded-lg cursor-pointer"
+              className="flex-1 flex justify-center items-center gap-3 p-1 border rounded-lg cursor-pointer"
             >
               <img
                 src={appleIcon}
                 alt="apple icon"
-                className="h-5 w-5 object-contain"
+                className="h-6 w-6 object-contain"
               />
-              <span className="font-text text-xs font-medium text-nowrap">Sign in with Apple</span>
+              <span className="font-text text-[13px] font-medium text-nowrap">Sign in with Apple</span>
             </button>
           </span>
           {/* option to sign in */}
-          <span className="flex justify-center items-center gap-2 font-text text-sm font-medium text-nowrap">
+          <span className="flex justify-center items-center gap-3 font-text text-[15px] font-medium text-nowrap">
             Have an account?  <button className="text-blue-800 cursor-pointer">Sign In</button>
           </span>
         </div>
       </div>
     </section>
     {/* Display sign up image */}
-    <section className="w-full md:flex-1 flex justify-center md:justify-end">
-      <img
-        src={sideImage}
-        alt="sing-up side image"
-        className="w-auto max-h-[480px] md:max-h-screen sm:h-auto object-cover"
-      />
+    {/* Add a background image with tailwind css bg-[url()] and other background image design */}
+    <section className="flex-1 bg-no-repeat bg-cover bg-center rounded-t-[45px] md:rounded-l-[45px] min-w-[480px] min-h-[480px]" style={{ backgroundImage: `url(${sideImage})` }}>      
     </section>
   </div>;
 };
